0ba6406f9cd933b12723429d2c3a2fd3a778e9e9
Pablo Corbalán <pablo.corbalan@protonmail.com>
2021-03-24 08:44:13 +0100

    script added and testing commits

---
+++ core/run.sh
@@ -0,0 +1,9 @@
+# This script is the main script for running the attack itself, without
+# any kind of interface.
+# This script can be run directly from the base directory if you do something
+# like: bash run.sh "route to the attack", but it's not the best option, you should
+# run the tewcrack.sh option, and it will check a lot of things that are needed to
+# run this script, then you can select the second option and that's it!
+# Good luck and use the script at your own risk!
+# For more information check out: github.com/pablocorbalann/tewcrack.sh
+echo "run"
